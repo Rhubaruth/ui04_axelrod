e01f951102b4eb946a8555eeec10b3ec2722f28f
Rhubaruth <kubik.vana@gmail.com>
2024-03-27 09:08:04 +0100

    axl library, simple output

---
+++ algs.py
@@ -0,0 +1,86 @@
+from random import Random
+import axelrod as axl
+
+# Dictionary with possible choices for a player
+CHOICES = {
+    'defect': 0,
+    'cooperate': 1
+}
+
+
+class Test(axl.Player):
+    """A player who only ever cooperates.
+
+    Names:
+
+    - Cooperator: [Axelrod1984]_
+    - ALLC: [Press2012]_
+    - Always cooperate: [Mittal2009]_
+    """
+
+    name = "Test TFT"
+    classifier = {
+        "memory_depth": 0,
+        "stochastic": False,
+        "long_run_time": False,
+        "inspects_source": False,
+        "manipulates_source": False,
+        "manipulates_state": False,
+    }
+
+    @staticmethod
+    def strategy(opponent: axl.Player) -> axl.Action:
+        """Actual strategy definition that determines player's action."""
+        if len(opponent.history) < 1:
+            return axl.Action.C
+        return opponent.history[-1]
+
+
+class SeededRandom(axl.Player):
+    """A player who only ever cooperates.
+
+    Names:
+
+    - Cooperator: [Axelrod1984]_
+    - ALLC: [Press2012]_
+    - Always cooperate: [Mittal2009]_
+    """
+
+    name = "Random seed"
+    classifier = {
+        "memory_depth": 0,
+        "stochastic": True,
+        "long_run_time": False,
+        "inspects_source": False,
+        "manipulates_source": False,
+        "manipulates_state": False,
+    }
+
+    actions = [axl.Action.C, axl.Action.D]
+
+    def __init__(self, seed: int):
+        super().__init__()
+        self.rng: Random = Random()
+        self.rng.seed(seed)
+
+    def strategy(self, opponent: axl.Player) -> axl.Action:
+        """Actual strategy definition that determines player's action."""
+        return self.rng.choice(self.actions)
+
+
+# Tit for tat strategy
+def TitForTat(history):
+    if len(history) == 0:
+        return CHOICES['cooperate']
+    else:
+        return history[-1][1]
+
+
+# Always defect strategy
+def AlwaysDefect(history):
+    return CHOICES['defect']
+
+
+# Always coop strategy
+def AlwaysCoop(history):
+    return CHOICES['cooperate']
---
+++ main.py
@@ -0,0 +1,128 @@
+import matplotlib.pyplot as plt
+import axelrod as axl
+from prettytable import PrettyTable
+from algs import TitForTat, \
+        AlwaysDefect, \
+        AlwaysCoop, \
+        Test, \
+        SeededRandom
+
+
+# Payoff matrix
+matrix = [[(1, 1), (5, 0)], [(0, 5), (3, 3)]]
+
+
+# Play for 200 rounds
+def battle(alg1, alg2):
+    # History will be in a form of:
+    # [(round 1 player1 choice, round 1 player2 choice),
+    #  (round 2 player1 choice, round 2 player2 choice), ...]
+    history = []
+    score = {
+        'player1': 0,
+        'player2': 0
+    }
+
+    for i in range(5):
+        # player2 should get history in a format:
+        # (player2 choice, player1 choice)
+        reversedHistory = [tup[::-1] for tup in history]
+
+        # get a players choices
+        player1_choice = alg1(history)
+        player2_choice = alg2(reversedHistory)
+
+        # append the current round to history
+        history.append((player1_choice,
+                        player2_choice))
+
+        # get scores
+        score1, score2 = matrix[player1_choice][player2_choice]
+        score['player1'] += score1
+        score['player2'] += score2
+
+    # print('Round', i+1, ':', history[-1], '- score', score1, ':', score2)
+    return score
+
+
+def main():
+    tournament_algs = [
+        TitForTat,
+        AlwaysDefect,
+        AlwaysCoop,
+        axl.TitForTat,
+        ]
+
+    print(f"Tournament with {len(tournament_algs)} participants")
+    for alg1 in tournament_algs:
+        for alg2 in tournament_algs:
+            print(f"{alg1.__name__} vs {alg2.__name__}: ", end=' ')
+            print(battle(alg1, alg2))
+            if alg1 == alg2:
+                break
+
+    # # Print results
+    # print(battle(AlwaysDefect, TitForTat))
+
+
+def mainAxl():
+    players = (
+            axl.Cooperator(),
+            axl.Defector(),
+            Test(),
+            axl.Alternator(),
+            axl.TitForTat(),
+            axl.CooperatorHunter(),
+            axl.Adaptive(),
+            SeededRandom(404)
+            )
+    tournament = axl.Tournament(players, turns=100, repetitions=3)
+    results: axl.ResultSet = tournament.play()
+    # print(*results.summarise(), sep='\n')
+    # print(*results.scores, sep='\n')
+    for idx in results.ranking:
+        print(results.players[idx], ' ', results.normalised_cooperation[idx])
+
+    # plot = axl.Plot(results)
+    # p = plot.boxplot()
+    # p.set_label('boxplot')
+
+    # p = plot.payoff()
+    # p.set_label('payoff')
+
+    # p = plot.winplot()
+    # p.set_label('winplot')
+    # p.show()
+
+    # plt.show()
+
+    table_payoffs = PrettyTable()
+    table_payoffs.field_names = [""] + results.ranked_names
+    for idx in results.ranking:
+        sorted_payoffs = [f'{results.payoffs[idx][j].pop():.3f}'
+                          for j in results.ranking]
+        row = [results.players[idx]] + sorted_payoffs
+        table_payoffs.add_row(row)
+
+    table_payoffs.align = "r"
+    table_payoffs.align[""] = "l"
+
+    print(table_payoffs)
+
+    table_coop = PrettyTable()
+    table_coop.field_names = [""] + results.ranked_names
+    for idx in results.ranking:
+        sorted_coop = [f'{results.normalised_cooperation[idx][j]:.3f}'
+                       for j in results.ranking]
+        row = [results.players[idx]] + sorted_coop
+        table_coop.add_row(row)
+
+    table_coop.align = "r"
+    table_coop.align[""] = "l"
+
+    print(table_coop)
+
+
+if __name__ == '__main__':
+    # main()
+    mainAxl()
